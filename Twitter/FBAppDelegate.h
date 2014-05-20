c1fee718f269d39fd4dad98b9f5e0d054ae84329
Muhammed Fatih Bulut <bulutmf@gmail.com>
2014-05-20 17:45:45 -0400

    Initial commit

---
+++ Twitter/FBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FBAppDelegate.h
+//  Twitter
+//
+//  Created by mfb on 5/20/14.
+//  Copyright (c) 2014 bulutmf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Twitter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Twitter
+//
+//  Created by mfb on 5/20/14.
+//  Copyright (c) 2014 bulutmf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FBAppDelegate class]));
+    }
+}
